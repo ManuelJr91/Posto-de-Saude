646c9ae4a576f76987df09447b25512e99c8f090
manuel.maloajunior@gmail.com <112087506+SugarDady8@users.noreply.github.com>
2022-10-22 19:28:03 -0100

    Commit de tela de Boas vindas

---
+++ src/view/Splash.java
@@ -0,0 +1,29 @@
+package view;
+
+
+public class Splash {
+
+    public static void main(String[] args) {
+       
+        SplashHome splash = new SplashHome();
+        splash.setVisible(true);
+        Login_Form login = new Login_Form();
+        
+         try {
+                    for(int i = 0; i<= 100; i++){
+                        Thread.sleep(40);
+                        splash.loadingNumber.setText(Integer.toString(i)+"%");
+                        splash.loadbar.setValue(i);
+                        if(i==100){
+                          splash.setVisible(false);
+                          login.setVisible(true);
+                        }
+                    }
+                    
+                } catch (Exception e) {
+                }
+        
+    }
+
+    
+}
